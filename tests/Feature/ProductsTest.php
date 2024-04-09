1ed945f337ff6de3423fb5d91eaaa7e8becf6e54
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-09 15:01:09 -0300

    feat: products tests

---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_products_pages_contains_empty_table()
+    {
+        $response = $this->get("/products");
+        
+        $response->assertSee("No products found");
+        $response->assertStatus(200);
+    }
+
+    public function test_products_pages_non_empty_table()
+    {
+        $product = Product::create([
+            "name" => "Product 1",
+            "price" => "123"
+        ]);
+
+        $response = $this->get("/products");
+        
+        $response->assertStatus(200);
+        $response->assertDontSee("No products found");
+        $response->assertViewHas("products", function($collection) use($product){
+            return $collection->contains($product);
+        });
+    }
+
+    public function test_paginated_products_table_doesnt_contain_11th_record()
+    {
+        $products = Product::factory(11)->create();
+        $lastProduct = $products->last();
+
+        $response = $this->get("/products");
+        $response->assertStatus(200);
+        $response->assertViewHas("products", function($collection) use ($lastProduct){
+            return !$collection->contains($lastProduct);
+        });
+    }
+}
