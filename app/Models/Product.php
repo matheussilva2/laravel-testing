38825b9481dc82c3e82a8b044a085fbb6bc7dde5
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-09 14:59:55 -0300

    feat: get converted price function

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\CurrencyService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ["name", "price"];
+
+    public function getPriceEurAttribute()
+    {
+        return (new CurrencyService())->convert($this->price, "usd", "eur");
+    }
 }
