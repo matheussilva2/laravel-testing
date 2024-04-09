c97b1ddc467025adb3c18349c114a544dcb123f1
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-09 15:01:17 -0300

    feat: currency conversion test

---
+++ tests/Unit/CurrencyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\CurrencyService;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    public function test_convert_test_usd_to_eur_successfully()
+    {
+        $result = (new CurrencyService())->convert(100, "usd", "eur");
+        $this->assertEquals(98, $result);
+    }
+
+    public function test_convert_test_usd_to_gbp_returns_zero()
+    {
+        $result = (new CurrencyService())->convert(100, "usd", "gbp");
+        $this->assertEquals(0, $result);
+    }
+}
