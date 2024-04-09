594c1e5763d92875042a5d68234208d7ac5b3617
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-09 12:20:37 -0300

    feat: project init

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_homepage_return_successful_response()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_documentation()
+    {
+        $response = $this->get("/");
+
+        $response->assertSee("Documentation");
+        $response->assertStatus(200);
+    }
+
+    public function test_homepage_contains_laracasts()
+    {
+        $response = $this->get("/");
+
+        $response->assertSee("Laracasts");
+        $response->assertStatus(200);
+    }
+
+    public function test_homepage_contains_laravel_news()
+    {
+        $response = $this->get("/");
+        $response->assertSee("Laravel News");
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_vibrant_ecosystem()
+    {
+        $response = $this->get("/");
+        $response->assertSee("Vibrant Ecosystem");
+        $response->assertStatus(200);
+    }
+}
