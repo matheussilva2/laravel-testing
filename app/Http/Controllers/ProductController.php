f647b210dda2df286acf703e361a671e2c4b0163
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-09 12:29:12 -0300

    feat: product controller index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index() : View {
+        $products = Product::all();
+
+        return view("products.index", compact("products"));
+    }
 }
