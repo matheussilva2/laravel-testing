63215591b49e4aeaf87dccc52919ca4d5fda3c9c
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-10 18:29:49 -0300

    feat: implement layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,33 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-<body>
-    <table class="table my-5">
-        <thead>
-            <tr>
-                <td>Name</td>
-                <td>Price</td>
-            </tr>
-        </thead>
-        <tbody class="bg-white divide-y">
-            @forelse($products as $product)
-                <tr class="bg-white">
-                    <td class="px-6 py-4 whitespace-no-wrap">{{$product->name}}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap">{{$product->price}}</td>
+@extends('layouts.app')
+
+@section('content')
+    <div class="mx-5 bg-light">
+        @if(auth()->user()->is_admin)
+        <a href="/products/create" class="btn btn-primary">Add new product</a>
+        @endif
+        <table class="table my-5 table-dark">
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Price</td>
                 </tr>
-            @empty
-                <tr class="bg-white">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm">
-                        {{ __("No products found") }}
-                    </td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
-</body>
-</html>
+            </thead>
+            <tbody class="bg-white divide-y">
+                @forelse($products as $product)
+                    <tr class="bg-white">
+                        <td class="px-6 py-4 whitespace-no-wrap">{{$product->name}}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap">{{$product->price}}</td>
+                    </tr>
+                @empty
+                    <tr class="bg-white">
+                        <td class="px-6 py-4 whitespace-no-wrap text-sm">
+                            {{ __("No products found") }}
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
