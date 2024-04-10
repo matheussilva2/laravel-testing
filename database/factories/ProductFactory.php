0c91fe032d8ce15ead39395d57b3d7c31b623b47
Matheus <matheusnascimentoeoq@gmail.com>
2024-04-10 16:17:29 -0300

    fix: dont invoke faker as a function

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,21 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Foundation\Testing\WithFaker;
 
 class ProductFactory extends Factory
 {
-    use WithFaker;
-    
-    public function __construct()
-    {
-        $this->setUpFaker();
-    }
-
     public function definition()
     {
         return [
-            "name" => $this->faker()->text(20),
+            "name" => $this->faker->text(20),
             "price" => rand(100, 999)
         ];
     }
